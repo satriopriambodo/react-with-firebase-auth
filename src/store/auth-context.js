215410eb804a1464a8e091ea120cce96e4f11aa5
satrio priambodo <satriopriambodo96@gmail.com>
2022-09-19 17:00:03 +0700

    register and login

---
+++ src/store/auth-context.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+const AuthContext = React.createContext({
+  token: "",
+  isLoggedIn: false,
+  login: (token) => {},
+  logout: () => {},
+});
+
+export const AuthContextProvider = (props) => {
+  const [token, setToken] = useState(null);
+  const userIsLoggedIn = !!token; //if token is string and not empty = true && token is string and empty = false
+
+  const loginHandler = (token) => {
+    setToken(token);
+  };
+  const logoutHandler = () => {
+    setToken(null);
+  };
+
+  const contextValue = {
+    token: token,
+    isLoggedIn: userIsLoggedIn,
+    login: loginHandler,
+    logout: logoutHandler,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
